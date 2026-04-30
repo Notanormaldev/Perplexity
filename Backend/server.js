6cb1fef5af711f26fa361ccccdf14c926d6714cd
Notanormaldev <harshpatel20050@gmail.com>
2026-04-30 21:41:54 +0530

    Add AI service integration and update dependencies

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 import app from "./src/app.js";
 import conntectDB from './src/config/database.js';
-
+import { testai } from './src/services/Ai.service.js';
+testai()
 conntectDB()
 app.listen(3000,()=>{
     console.log('3000 port pe conntected');
