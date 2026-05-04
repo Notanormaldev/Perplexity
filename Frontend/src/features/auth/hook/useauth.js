dca36b6266eabfd4979310dada9f46f9f51fad10
Notanormaldev <harshpatel20050@gmail.com>
2026-05-04 16:14:07 +0530

    Hook layer

---
+++ Frontend/src/features/auth/hook/useauth.js
@@ -0,0 +1,50 @@
+import { useDispatch } from "react-redux";
+import { getme, login, register } from "../services/auth.api";
+import { seterror, setloading, setuser } from "../auth.slice";
+
+
+
+
+export  function useauth(){
+
+    const dispatch = useDispatch()
+
+    async function handleregister({email,username,password}){
+        try {
+            dispatch(setloading(true))
+            const data = register({email,username,password})
+        } catch (error) {
+            dispatch(seterror(error.response?.data?.message || "Register failed"))
+        }finally{
+           dispatch(setloading(false))
+        }
+        
+   }
+    async function handlelogin({email,password}){
+        try {
+            dispatch(setloading(true))
+            const data = login({email,password})
+            dispatch(setuser(data.user))
+        } catch (error) {
+            dispatch(seterror(error.response?.data?.message || "Login failed"))
+        }finally{
+           dispatch(setloading(false))
+        }
+        
+   }
+    async function handlegetme(){
+        try {
+            dispatch(setloading(true))
+            const data = getme()
+        } catch (error) {
+            dispatch(seterror(error.response?.data?.message || "get-me failed"))
+        }finally{
+           dispatch(setloading(false))
+        }
+        
+   }
+
+   return ({handlegetme,handlelogin,handleregister})
+
+
+}
