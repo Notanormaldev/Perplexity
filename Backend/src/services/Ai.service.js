697f1a078d4e906bf935562addb3656f913bc8f3
Notanormaldev <harshpatel20050@gmail.com>
2026-05-05 14:44:05 +0530

    Add Mistral AI integration; update dependencies and implement chat title generation

--- Backend/src/services/Ai.service.js
+++ Backend/src/services/Ai.service.js
@@ -1,11 +1,16 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import {HumanMessage} from 'langchain'
+import {HumanMessage, SystemMessage} from 'langchain'
+
+import{ChatMistralAI}  from '@langchain/mistralai'
 const geminimodel = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
   apiKey: process.env.GEMENI_API
 });
 
-
+const mistralmodel = new ChatMistralAI({
+  model:"mistral-small-latest",
+  apiKey:process.env.MISTRAL_API
+})
 
 
 export async function generateMessage(message){
@@ -14,4 +19,11 @@ export async function generateMessage(message){
   ]
   )
   return res.text
+}
+export async function generateChatTitle(message){
+    const res = await mistralmodel.invoke([
+      new SystemMessage(`you are a helpful assistant that generates a title for a chat conversation based on use message, the title should be short and descriptive of the conversation, and should not contain more than 5 words`),
+      new HumanMessage(message)
+    ])
+    return res.text
 }
