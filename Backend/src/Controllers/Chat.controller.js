9591947144776b8f38ef9dfc0c5790f31d1d11cb
Notanormaldev <harshpatel20050@gmail.com>
2026-05-05 14:46:11 +0530

    Add chat title generation to message response, update messageandres function

--- Backend/src/Controllers/Chat.controller.js
+++ Backend/src/Controllers/Chat.controller.js
@@ -1,12 +1,15 @@
-import { generateMessage } from "../services/Ai.service.js"
+import { generateChatTitle, generateMessage } from "../services/Ai.service.js"
 
 export async function messageandres(req,res){
     const {message} = req.body
     
     const result = await generateMessage(message)
-
+    const title = await generateChatTitle(message)
+    console.log(title);
+    
     res.send({
-        AImessage:result
+        AImessage:result,
+        title
     })
 
 }
