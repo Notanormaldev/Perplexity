768a3811675ee7d27123ca9af03080c6e215d6b1
Notanormaldev <harshpatel20050@gmail.com>
2026-05-13 17:21:20 +0530

    Add upload route to handle file uploads in chatRouter

--- Backend/src/Routes/chat.route.js
+++ Backend/src/Routes/chat.route.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { authuser } from "../middleware/authuser.js";
 import { deletechat, getchats, getmessages, messageandres } from "../Controllers/Chat.controller.js";
+import { upload } from "../middleware/upload.middleware.js";
+import { uploadDocument } from "../Controllers/document.controller.js";
 
 
 
@@ -9,7 +11,7 @@ chatRouter.post('/message',authuser,messageandres)
 chatRouter.get('/',authuser,getchats)
 chatRouter.get('/messages/:chatid',authuser,getmessages)
 chatRouter.delete('/delete/:chatid',authuser,deletechat)
-
+chatRouter.post('/upload/:chatid',authuser,upload.single('file'),uploadDocument)
 
 
 export default chatRouter;
