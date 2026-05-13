2be2fbb26f64e777cd80e84ca8082b06267a53a8
Notanormaldev <harshpatel20050@gmail.com>
2026-05-13 17:17:59 +0530

    Implement uploadDocument function to handle document uploads and text extraction

---
+++ Backend/src/Controllers/document.controller.js
@@ -0,0 +1,44 @@
+import documentmodel from "../model/document.model.js"
+
+import { extractText }
+from "../services/file.service.js"
+
+import { chunkText }
+from "../services/rag.service.js"
+
+import { createEmbedding }
+from "../services/embedding.service.js"
+
+export async function uploadDocument(req,res){
+
+   const file = req.file
+
+   const {chatid} = req.params
+
+   // extract text
+   const text = await extractText(file)
+
+   // chunks
+   const chunks = chunkText(text)
+
+   // save all chunks
+   for(const chunk of chunks){
+
+      const embedding =
+      await createEmbedding(chunk)
+
+      await documentmodel.create({
+
+         chat:chatid,
+
+         chunk,
+
+         embedding
+      })
+   }
+
+   return res.status(200).json({
+
+      msg:"Document uploaded successfully"
+   })
+}
