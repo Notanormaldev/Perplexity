36197a87011bdcb4de5e46129e53a4bf423bd49d
Notanormaldev <harshpatel20050@gmail.com>
2026-05-13 17:39:20 +0530

    Refactor searchRelevantChunks function to utilize vector search for relevant document chunks

--- Backend/src/services/rag.service.js
+++ Backend/src/services/rag.service.js
@@ -1,19 +1,49 @@
-export function chunkText(text){
+import mongoose from "mongoose"
 
-   const chunkSize = 500
+import documentmodel
+from "../model/document.model.js"
 
-   const chunks = []
+import { createEmbedding }
+from "./embedding.service.js"
 
-   for(
-      let i=0;
-      i<text.length;
-      i+=chunkSize
-   ){
+export async function searchRelevantChunks(
 
-      chunks.push(
-         text.slice(i,i+chunkSize)
-      )
-   }
+   chatid,
+   question
 
-   return chunks
+){
+
+   const embedding =
+   await createEmbedding(question)
+
+   const result =
+   await documentmodel.aggregate([
+
+      {
+         $vectorSearch:{
+
+            index:"vector_index",
+
+            path:"embedding",
+
+            queryVector:embedding,
+
+            numCandidates:50,
+
+            limit:3,
+
+            filter:{
+
+               chat:
+               new mongoose.Types.ObjectId(
+                  chatid
+               )
+            }
+         }
+      }
+   ])
+
+   return result
+   .map(doc=>doc.chunk)
+   .join("\n")
 }
