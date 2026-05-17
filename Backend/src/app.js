d52ef54993263f2ab8cf4b94815a6de5475679aa
Notanormaldev <harshpatel20050@gmail.com>
2026-05-17 17:01:31 +0530

    fix: refactor CORS middleware configuration in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,19 +4,20 @@ import UserRoute from './Routes/user.route.js'
 import morgan from 'morgan'
 import cors from 'cors'
 import chatRouter from './Routes/chat.route.js'
-
-
 const app = express()
+app.use(cors({
+    origin:"https://zerio-ai.onrender.com",
+    credentials:true,
+    methods:['GET','POST','DELETE','PUT'],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
 app.use(express.json())
 app.use(cookie())
 app.use(morgan('dev'))
 
 
-app.use(cors({
-    origin:"https://zerio-ai.onrender.com",
-    credentials:true,
-    methods:['GET','POST','DELETE','PUT']
-}))
+
 app.use('/api/auth',UserRoute)
 app.use('/api/chats',chatRouter)
 
