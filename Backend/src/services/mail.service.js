bc124e7abf450809ed2e4628aa80677023bcf2f8
Notanormaldev <harshpatel20050@gmail.com>
2026-05-17 16:16:17 +0530

    fix: update Brevo API integration and refactor email sending logic

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -40,12 +40,12 @@
 //     // console.log('email sent',dts);
     
 // }
-import * as Brevo from "@getbrevo/brevo";
-
-
 const client = new Brevo.TransactionalEmailsApi();
+import { TransactionalEmailsApi, TransactionalEmailsApiApiKeys, SendSmtpEmail } from "@getbrevo/brevo";
+
+const client = new TransactionalEmailsApi();
 client.setApiKey(
-  Brevo.TransactionalEmailsApiApiKeys.apiKey,
+  TransactionalEmailsApiApiKeys.apiKey,
   process.env.BREVO_API_KEY
 );
 
@@ -54,16 +54,15 @@ client.getAccount()
   .then(() => console.log("Email server is ready to send messages"))
   .catch((error) => console.error("Error connecting to email server:", error));
 
-
 export async function sendEmail({ to, html, subject, text }) {
-  const mailOptions = {
-    sender: { email: process.env.EMAIL },
-    to: [{ email: to }],                 
-    subject,
-    htmlContent: html,
-    textContent: text,
-  };
+  const mailOptions = new SendSmtpEmail();
+  
+  mailOptions.sender = { email: process.env.EMAIL };
+  mailOptions.to = [{ email: to }];
+  mailOptions.subject = subject;
+  mailOptions.htmlContent = html;
+  mailOptions.textContent = text;
 
   const dts = await client.sendTransacEmail(mailOptions);
-  return dts;  
+  return dts;
 }
