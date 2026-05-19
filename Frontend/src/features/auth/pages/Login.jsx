8b0cb667e22af5a32a901d90b79710c1b5ad29dc
Notanormaldev <harshpatel20050@gmail.com>
2026-05-19 08:36:55 +0530

    fix: update layout for Login and Register components to center icon elements

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -52,7 +52,7 @@ function Login() {
        <h1 className='text-white absolute left-1 top-1 font-light font-stretch-50% text-3xl mask-b-from-neutral-50 '><i  class="ri-ancient-gate-line"></i>  ZErio Ai</h1>
       <div className="w-full max-w-md">
         <div className="rounded-[32px] border border-slate-800 bg-black-900/95 p-8 shadow-2xl shadow-slate-950/30 backdrop-blur-xl">
-       <div className='font-extrabold font-stretch-50% text-3xl sm:text-4xl mask-b-from-neutral-50 w-12 sm:w-16 m-auto mb-4'>
+       <div className='font-extrabold font-stretch-50% text-3xl sm:text-4xl mask-b-from-neutral-50 w-12 sm:w-16 mx-auto mb-4 flex justify-center items-center'>
          <i class="ri-ancient-gate-line"></i> 
        </div>
           <div className="mb-8 flex items-center justify-between gap-4">
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -68,8 +68,8 @@ function Register() {
       <h1 className='text-white absolute left-1 top-1 font-light font-stretch-50% text-3xl mask-b-from-neutral-50 font-family:'><i class="ri-ancient-gate-line"></i>  ZErio Ai</h1>
       <div className="w-full max-w-md relative z-10">
         <div className="rounded-[32px] border border-slate-800 bg-black-900/95 p-8 shadow-2xl shadow-slate-950/30 backdrop-blur-xl">
-          <div className='font-extrabold mb-5 font-stretch-50% text-3xl sm:text-4xl mask-b-from-neutral-50 w-12 sm:w-16 m-auto'>
-            <i class="ri-ancient-gate-line "></i>
+          <div className='font-extrabold mb-5 font-stretch-50% text-3xl sm:text-4xl mask-b-from-neutral-50 w-12 sm:w-16 mx-auto flex justify-center items-center'>
+            <i class="ri-ancient-gate-line"></i>
           </div>
           <div className="mb-8 flex items-center justify-between gap-4">
             <div>
