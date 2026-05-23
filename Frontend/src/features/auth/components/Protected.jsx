d1ca4d3b20c3501ce7a559358b71fc31ae724fd2
Notanormaldev <harshpatel20050@gmail.com>
2026-05-23 19:07:55 +0530

    improve loading state

--- Frontend/src/features/auth/components/Protected.jsx
+++ Frontend/src/features/auth/components/Protected.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router'
+import Loader from '../../../components/Loader'
 
 function Protected({children}) {
 
    const {user,loading} = useSelector(state=>state.auth)
 
   if(loading){
-    return <h1 className='text-4xl '>Loading..</h1>
+    return <Loader />
   }
 
   if(!user){
